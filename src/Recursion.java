97f25f77ffd46ad24ad0af1378fed4e14cade999
code-bajju <11212766@mmec.mmdu.org>
2024-08-07 22:39:40 +0530

    fibonacci

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,10 +1,9 @@
 public class Recursion {
     public static void main(String[] args) {
        System.out.println(factorial(8));
+       System.out.println(fibonacci(10));
     }
 
-    // factorial
-    
     static int factorial(int n){
         if (n==0 || n ==1){
             return 1;
@@ -14,5 +13,18 @@ public class Recursion {
             return n * factorial(n-1);
         }
 
+    }
+
+    // fibbonacci series
+
+    static int fibonacci(int n){
+        int i,t=0,t2=1,nextterm;
+        for (i=0;i<n;i++){
+            nextterm = t+t2;
+            t = t2;
+            t2 = nextterm;
+            System.out.println(t2);
+        }
+        return t2; 
     }
 }
